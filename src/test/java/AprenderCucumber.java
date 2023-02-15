2b3cf700a46ef9d43ca427588b257ce695cf3a9f
henriquemj <henriquemadureira05@gmail.com>
2023-02-15 19:29:22 -0300

    Cenário Contador

--- src/test/java/AprenderCucumber.java
+++ src/test/java/AprenderCucumber.java
@@ -16,4 +16,23 @@ public class AprenderCucumber {
 	public void a_especificação_deve_finalizar_com_sucesso() throws Throwable {
 	}
 	
+	private int contador = 0;
+	
+	@Dado("^que o valor do contador é (\\d+)$")
+	public void que_o_valor_do_contador_é(int arg1) throws Throwable {
+		contador = arg1;
+	}
+
+	@Quando("^eu incrementar em (\\d+)$")
+	public void eu_incrementar_em(int arg1) throws Throwable {
+	    contador = contador + arg1;
+	}
+
+	@Então("^o valor do contador será (\\d+)$")
+	public void o_valor_do_contador_será(int arg1) throws Throwable {
+	    System.out.println(arg1);
+	    System.out.println(contador);
+	    System.out.println(arg1 == contador);
+	}
+	
 }
--- src/test/java/aprender_cucumber.feature
+++ src/test/java/aprender_cucumber.feature
@@ -9,4 +9,8 @@ Cenário: Deve executar especificação
 	Dado que criei o arquivo corretamente
 	Quando executá-lo
 	Então a especificação deve finalizar com sucesso
-	Então qualquer coisa
+	
+Cenário: Deve incrementar contador
+	Dado que o valor do contador é 15
+	Quando eu incrementar em 3
+	Então o valor do contador será 18
